1636b7725b6575d2063bf7d9861567cedf9ec26f
dzalf <dzalf@yahoo.com>
2019-01-13 15:43:02 +0000

    Update LM95071_Single_Sensor.ino
    Added comments and changed pin var types

--- examples/LM95071_Single_Sensor/LM95071_Single_Sensor.ino
+++ examples/LM95071_Single_Sensor/LM95071_Single_Sensor.ino
@@ -1,27 +1,27 @@
 #include "LM95071.h"
 
 float temperature;
-int SSP = 10;
-int LED = 14;
 
-LM95071 SPI_Sensor(SSP, DEBUG_OFF);
+const int SSP = 10;
+const int LED = 14;
+
+LM95071 SPI_Sensor(SSP, DEBUG_OFF); // Instantiate a new LM95071 
 
 void setup() {
 
   Serial.begin(115200);
-
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
+  
   SPI_Sensor.begin();
 }
 
-
 void loop() {
 
-  delay(150);
+  delay(50);
   temperature =  SPI_Sensor.getTemperature();
 
-  Serial.print("Current temp = ");
+  Serial.print("Current temperature = ");
   Serial.print(temperature);
   Serial.print(char(176));
   Serial.println("C");
@@ -30,6 +30,6 @@ void loop() {
   delay(50);
 
   digitalWrite(LED, LOW);
-  delay(150);
+  delay(150);             // Long-ish delay for visualization purposes...flashy response
 
 }
